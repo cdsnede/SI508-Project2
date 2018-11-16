c45d023d670c983109c058a92c628a65ef969f84
Courtney Snede <cdsnede@umich.edu>
2018-11-16 16:39:19 -0500

    NPS parsing done, creates national site instances

--- proj2_nps.py
+++ proj2_nps.py
@@ -1,7 +1,13 @@
-## proj_nps.py
-## Skeleton for Project 2, Fall 2018
-## ~~~ modify this file, but don't rename it ~~~
 from secrets import google_places_key
+from alternate_advanced_caching import *
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import json
+
+####################
+## set up classes ##
+####################
 
 ## you can, and should add to and modify this class any way you see fit
 ## you can add attributes and modify the __init__ parameters,
@@ -15,20 +21,64 @@ class NationalSite():
         self.description = desc
         self.url = url
 
-        # needs to be changed, obvi.
-        self.address_street = '123 Main St.'
-        self.address_city = 'Smallville'
-        self.address_state = 'KS'
-        self.address_zip = '11111'
+        address = requests.get(base+url).text
+        addressSoup=BeautifulSoup(address, 'html.parser')
+        addressP=addressSoup.find('p', class_='adr')
+        self.address_street = addressP.find('span', itemprop='streetAddress').text
+        self.address_city = addressP.find('span', itemprop='addressLocality').text
+        self.address_state = addressP.find('span', itemprop='addressRegion').text
+        self.address_zip = addressP.find('span', itemprop='postalCode').text
 
-## you can, and should add to and modify this class any way you see fit
-## you can add attributes and modify the __init__ parameters,
-##   as long as tests still pass
-##
-## the starter code is here just to make the tests run (and fail)
+    def __str__(self):
+        #need to add in self.address_street - has weird line break
+        return "{}, ({}): {}, {}, {} {}".format(self.name, self.type, self.address_street, self.address_city, self.address_state, self.address_zip)
+
+
 class NearbyPlace():
     def __init__(self, name):
         self.name = name
+
+
+############################
+## homepage cache and run ##
+############################
+cache_file = "nps.json"
+site="nps"
+topic="national_sites"
+cache = Cache(cache_file)
+base = "https://www.nps.gov"
+
+def create_id(site, topic):
+    return "{}_{}.json".format(site, topic)
+
+UID = create_id(site, topic)
+response = cache.get(UID)
+
+if response == None:
+    response = requests.get(base).text
+    cache.set(UID, response, 7)
+
+def process_homepage(response):
+        #parse homepage, find state link, go to state link, retrieve list of parks
+        soup = BeautifulSoup(response, 'html.parser')
+        searchbar=soup.find('body').find_all('div', class_='SearchBar')
+        menu=searchbar[0].find('ul', class_='dropdown-menu')
+        links=menu.find_all('a')
+        stateurls=[]
+        slash='/'
+        for link in links:
+            stateurls.append(link.get('href'))
+        return stateurls
+
+stateurls=process_homepage(response)
+#######################
+#     get state info     #
+#######################
+#original do not delete
+#def create_id(site, topic):
+#    return "{}_{}_{}.json".format(site, topic, str(datetime.now()).replace(' ', ''))
+
+
 
 ## Must return the list of NationalSites for the specified state
 ## param: the 2-letter state abbreviation, lowercase
@@ -37,8 +87,32 @@ class NearbyPlace():
 ##        (e.g., National Parks, National Heritage Sites, etc.) that are listed
 ##        for the state at nps.gov
 def get_sites_for_state(state_abbr):
-    return []
+    #find state link, go to state link, retrieve list of parks
+    slash='/'
+    splitstate=[]
+    for state in stateurls:
+        splitstate.append(state.split("/"))
+    for i in splitstate:
+        if i[2] == state_abbr:
+            go_to_state=slash.join(i)
+            stateinfo = requests.get(base+go_to_state).text
+            statesoup=BeautifulSoup(stateinfo, 'html.parser')
+            parksoup=statesoup.find_all('ul',id="list_parks")
+            sites=parksoup[0].find_all('li', class_='clearfix')
+            parklist=[]
+            for site in sites:
+                type=site.find('h2').text
+                name=site.find('h3').find('a').text
+                desc=site.find('p').text
+                parkurl=site.find('h3').find('a').get('href')
+                park = NationalSite(type, name, desc, parkurl)
+                parklist.append(park)
+            return parklist
 
+abbrev='ok'
+parklist=get_sites_for_state(abbrev)
+for park in parklist:
+    print(park)
 
 ## Must return the list of NearbyPlaces for the specifite NationalSite
 ## param: a NationalSite object

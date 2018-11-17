730d98e947921884613baab7e1f7dfa0fe58791e
Courtney Snede <cdsnede@umich.edu>
2018-11-17 10:51:43 -0500

    plotly works, not zoomed in tho

--- proj2_nps.py
+++ proj2_nps.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
 import json
-import plotly
+import plotly.plotly as py
 
 ####################
 ## set up classes ##
@@ -38,8 +38,10 @@ class NationalSite():
 
 
 class NearbyPlace():
-    def __init__(self, name):
+    def __init__(self, name, lng, lat):
         self.name = name
+        self.lng=lng
+        self.lat=lat
 
     def __str__(self):
         return self.name
@@ -160,6 +162,22 @@ parklist=get_sites_for_state(abbrev)
 #    - one to get the GPS coordinates for a site (tip: do a text search for <site.name> <site.type>, e.g. “Death Valley National Park” or “Sleeping Bear Dunes National Lakeshore” instead of “Death Valley” or “Sleeping Bear”, to ensure a more precise match--it turns out there are lots of places called “Death Valley” that aren’t National Parks!),
 #    - AND another one to get the places that are nearby that location.
  # - get_nearby_places(site_object) should return a list of `NearbyPlace` instances.
+def get_googleapi_coordinates(national_site):
+    paramsdict={}
+    paramsdict['key']='AIzaSyDQGvVsAJfiMnnzQBjsUW-9Oou_1Sx2PAc'
+    paramsdict['input']='{} {}'.format(national_site.name, national_site.type)
+    paramsdict['inputtype']='textquery'
+    paramsdict['fields']='geometry'
+    site_base = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+    placedata=check_cache(site_base, paramsdict)
+    findplace=json.loads(placedata)
+    latitude=0
+    longitude=0
+    for place in findplace['candidates']:
+        latitude=place['geometry']['location']['lat']
+        longitude=place['geometry']['location']['lng']
+        #coords=str(latitude)+','+str(longitude)
+    return latitude,longitude
 
 def get_nearby_places_for_site(national_site):
     paramsdict={}
@@ -182,10 +200,20 @@ def get_nearby_places_for_site(national_site):
     nearby_base='https://maps.googleapis.com/maps/api/place/nearbysearch/json'
     nearby_data=check_cache(nearby_base, nearby_paramsdict)
     nearbyplaces=json.loads(nearby_data)
-    print(nearby_data) #not parsed yet, but works at geting 20 places
+    nearbyplaces_list=[]
+    for loc in nearbyplaces['results']:
+        lat=loc['geometry']['location']['lat']
+        lng=loc['geometry']['location']['lng']
+        name=loc['name']
+        nearbyplaces_list.append(NearbyPlace(name,lng,lat))
+    return nearbyplaces_list
 
 test_place=parklist[1]
-get_nearby_places_for_site(test_place)
+testcoords=get_googleapi_coordinates(test_place)
+testlist=get_nearby_places_for_site(test_place)
+
+#for l in testlist:
+#    print(l)
 ## Must plot all of the NationalSites listed for the state on nps.gov
 ## Note that some NationalSites might actually be located outside the state.
 ## If any NationalSites are not found by the Google Places API they should
@@ -194,7 +222,48 @@ get_nearby_places_for_site(test_place)
 ## returns: nothing
 ## side effects: launches a plotly page in the web browser
 def plot_sites_for_state(state_abbr):
-    pass
+    #need to make it skip long/lat coordinates that are zero
+    lon_vals=[]
+    lat_vals=[]
+    text_vals=[]
+    statelist=get_sites_for_state(state_abbr)
+    for site in statelist:
+        text_vals.append(site.name)
+        sitecoords=get_googleapi_coordinates(site)
+        lat_vals.append(sitecoords[0])
+        lon_vals.append(sitecoords[1])
+
+    #min_lat = 10000
+    #max_lat = -10000
+    #min_lon = 10000
+    #max_lon = -10000
+
+    #for str_v in lat_vals:
+    #    v = float(str_v)
+    #    if v < min_lat:
+    #        min_lat = v
+    #    if v > max_lat:
+    #        max_lat = v
+    #for str_v in lon_vals:
+    #    v = float(str_v)
+    #    if v < min_lon:
+    #        min_lon = v
+    #    if v > max_lon:
+    #        max_lon = v
+
+    #lat_axis = [min_lat -1, max_lat+1]
+    #lon_axis = [min_lon-1, max_lon+1]
+    #center_lat = (max_lat+min_lat) / 2
+    #center_lon = (max_lon+min_lon) / 2
+
+    data = [dict(type = 'scattergeo', locationmode = 'USA-states', lon = lon_vals, lat = lat_vals, text = text_vals, mode = 'markers', marker = dict(size = 8, symbol = 'star',))]
+
+    layout = dict(title = 'National Sites<br>(Hover for names)', geo = dict(scope='usa', projection=dict(type='albers usa'),showland = True, landcolor = "rgb(250, 250, 250)", subunitcolor = "rgb(100, 217, 217)", countrycolor = "rgb(217, 100, 217)", countrywidth = 3, subunitwidth = 3))
+
+    fig = dict(data=data, layout=layout)
+    py.plot(fig, validate=False, filename='National Sites by State')
+
+plot_sites_for_state(abbrev)
 
 ## Must plot up to 20 of the NearbyPlaces found using the Google Places API
 ## param: the NationalSite around which to search
